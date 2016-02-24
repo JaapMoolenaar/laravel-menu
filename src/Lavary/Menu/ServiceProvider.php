b363658a190a6489b6133e43cb97c3316d7f1909
Jaap Moolenaar <jaap@videosupport.me>
2016-02-24 11:08:13 +0100

    Blade extended from within service provider. Some windows servers had issues with requiring the blade file

--- src/Lavary/Menu/ServiceProvider.php
+++ src/Lavary/Menu/ServiceProvider.php
@@ -1,15 +1,9 @@
 <?php namespace Lavary\Menu;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Blade;
 
 class ServiceProvider extends BaseServiceProvider {
-
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = true;
 
 	/**
 	 * Register the service provider.
@@ -34,7 +28,7 @@ class ServiceProvider extends BaseServiceProvider {
 	public function boot()
 	{
 		// Extending Blade engine
-		require_once('blade/lm-attrs.php');
+		$this->registerBladeExtensions();
 
 		$this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-menu');
 
@@ -45,14 +39,46 @@ class ServiceProvider extends BaseServiceProvider {
 		]);
 	}
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('menu');
-	}
+    /**
+     * Register custom blade extensions
+     * 
+	 * @return void
+     */
+    protected function registerBladeExtensions() 
+    {
+        // @lm-attrs
+        // Buffers the output if there's any.	
+        // The output will be passed to mergeStatic() where it is merged with 
+        // item's attributes
+        Blade::extend( function($view, $compiler){
 
+            $pattern = '/(\s*)@lm-attrs\s*\((\$[^)]+)\)/';
+            return preg_replace($pattern, 
+                               '$1<?php $lm_attrs = $2->attr(); ob_start(); ?>',
+                                $view);
+        });
+
+        // @lm-endattrs 
+        // Reads the buffer data using ob_get_clean() and passes it to 
+        // MergeStatic(). mergeStatic() takes the static string, converts it 
+        // into a normal array and merges it with others.
+        Blade::extend( function($view, $compiler){
+
+            $pattern = $this->createPlainMatcher('lm-endattrs');
+            return preg_replace($pattern, 
+                               '$1<?php echo \Lavary\Menu\Builder::mergeStatic(ob_get_clean(), $lm_attrs); ?>$2', 
+                                $view);
+        });
+    }
+    
+    /**
+     * Create a plain Blade matcher.
+     *
+     * @param  string  $function
+     * @return string
+     */
+    protected function createPlainMatcher($function)
+    {
+        return '/(?<!\w)(\s*)@' . $function . '(\s*)/';
+    }
 }
